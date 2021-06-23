7705ec0f41270bbcb911b28c377620b689a7d63b
Diego Escobar <diego.netsolutions@gmail.com>
2021-06-23 07:46:26 -0400

    fixed redirects issues

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,8 @@ import { Controller, Get, Redirect } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  @Redirect('/', 301)
-  redirectToSwagger(): void {
+  @Redirect('/swagger', 301)
+  redirect(): void {
     return;
   }
 }

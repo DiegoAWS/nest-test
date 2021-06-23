42723c36032545913cabfd4715d063eca335bfeb
Diego Escobar <diego.netsolutions@gmail.com>
2021-06-23 06:29:16 -0400

    added Unitary tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,9 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let userController: UsersController;
+
+  let usersService: UsersService;
+
+  const mockUserDto: CreateUserDto = {
+    name: 'John',
+    lastName: 'Doe',
+    age: 25,
+    email: 'john.doe@gmail.com',
+    linkedinProfile: 'https://cl.linkedin.com/company/john-doe',
+  };
+  const mockUser: User = {
+    id: 'test_id',
+    created_at: Date.now().toString(),
+    ...mockUserDto,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +28,48 @@ describe('UsersController', () => {
       providers: [UsersService],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    userController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+  });
+  describe('testing defined', () => {
+    it('should be defined', () => {
+      expect(userController).toBeDefined();
+    });
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('testing create method', () => {
+    it('testing DI ', async () => {
+      const service = jest.spyOn(usersService, 'create');
+      service.mockImplementation(() => mockUser);
+
+      expect(await userController.create(mockUserDto)).toBe(mockUser);
+      expect(service).toHaveBeenCalledWith(mockUserDto);
+    });
+  });
+
+  describe('testing getAll', () => {
+    it('testing get All', async () => {
+      jest.spyOn(usersService, 'findAll').mockImplementation(() => [mockUser]);
+
+      const userCreated = await userController.create(mockUserDto);
+      const { id, created_at, ...userToTest } = userCreated;
+
+      expect(id).toBeDefined();
+      expect(created_at).toBeDefined();
+      expect(userToTest).toStrictEqual(mockUserDto);
+    });
+  });
+
+  describe('testing Remove', () => {
+    it('testing Remove', async () => {
+      const service = jest.spyOn(usersService, 'remove');
+
+      service.mockImplementation(() => [{ mockUser }]);
+
+      await userController.remove('test_id');
+
+      expect(await userController.remove('test_id')).toBe(undefined);
+      expect(service).toHaveBeenCalledWith('test_id');
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,13 +4,15 @@ import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
-  const mockUser: CreateUserDto = {
+
+  const mockUserDto: CreateUserDto = {
     name: 'John',
     lastName: 'Doe',
     age: 25,
     email: 'john.doe@gmail.com',
     linkedinProfile: 'https://cl.linkedin.com/company/john-doe',
   };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
@@ -23,6 +25,7 @@ describe('UsersService', () => {
     it('should be defined', () => {
       expect(service).toBeDefined();
     });
+
     it('users array should be declared and empty', () => {
       expect(service.usersStore).toEqual([]);
     });
@@ -30,19 +33,22 @@ describe('UsersService', () => {
 
   describe('testing create method', () => {
     let createUser;
+
     beforeEach(async () => {
-      createUser = service.create(mockUser);
+      createUser = service.create(mockUserDto);
     });
+
     it('change the usersStore array', () => {
       const { id, created_at } = createUser;
-      expect(service.usersStore).toEqual([{ id, created_at, ...mockUser }]);
+      expect(service.usersStore).toEqual([{ id, created_at, ...mockUserDto }]);
     });
+
     it('testing the return value', () => {
       const { id, created_at, ...testUser } = createUser;
       expect(typeof id).toBe('string');
       expect(id.length).toBe(36);
       expect(typeof created_at).toBe('string');
-      expect(testUser).toEqual(mockUser);
+      expect(testUser).toEqual(mockUserDto);
     });
   });
 
@@ -50,27 +56,29 @@ describe('UsersService', () => {
     it('testing method', () => {
       expect(service.findAll()).toEqual([]);
     });
-    describe('testing remove method', () => {
-      it('testing find and remove', () => {
-        service.usersStore = [
-          {
-            ...mockUser,
-            id: 'test_id',
-            created_at: 'test',
-          },
-        ];
-        expect(service.remove('test_id')).toBeUndefined();
-        expect(service.usersStore).toEqual([]);
-      });
-      it('testing not finding and throwing an exception', () => {
-        try {
-          service.remove('test_id');
-          // Fail test if above expression doesn't throw anything.
-          expect(true).toBe(false);
-        } catch (e) {
-          expect(e.message).toBe('Element not Found');
-        }
-      });
+  });
+
+  describe('testing remove method', () => {
+    it('testing find and remove', () => {
+      service.usersStore = [
+        {
+          ...mockUserDto,
+          id: 'test_id',
+          created_at: 'test',
+        },
+      ];
+      expect(service.remove('test_id')).toBeUndefined();
+      expect(service.usersStore).toEqual([]);
+    });
+
+    it('testing not finding and throwing an exception', () => {
+      try {
+        service.remove('test_id');
+        // Fail test if above expression doesn't throw anything.
+        expect(true).toBe(false);
+      } catch (e) {
+        expect(e.message).toBe('Element not Found');
+      }
     });
   });
 });
